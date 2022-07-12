33a10804cf72dc6ccf50677f77edfef8cba237cc
Florent Flament <contact@florentflament.com>
2022-07-12 19:58:26 +0200

    Add playfield graphic example

--- src/variables.asm
+++ src/variables.asm
@@ -1,3 +1,11 @@
 ;;; Sprites pointers
 sprite_a_ptr	DS.B	2
 sprite_b_ptr	DS.B	2
+
+;;; 40x40 playfield picture
+pfpic_p0	DS.B	2
+pfpic_p1	DS.B	2
+pfpic_p2	DS.B	2
+pfpic_p3	DS.B	2
+pfpic_p4	DS.B	2
+pfpic_p5	DS.B	2
