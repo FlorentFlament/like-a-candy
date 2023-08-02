93f60f0a58d422eaeda3a44aaad1a3e8625a50d0
Florent Flament <contact@florentflament.com>
2023-08-02 22:13:53 +0200

    Add tools to generate gfx data from png
    Also adds a gfx example.

---
+++ tools/png40pf.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# Based on m/tools/png2logo.py
+
+# This script converts a Black and White 40x40 picture from png format
+# to dasm format; to be used for Atari VCS 2600 demos.
+
+from os.path import basename
+import sys
+
+from PIL import Image
+
+import asmlib
+from imglib import *
+
+def sanity_check(im):
+    """Checks that the image has the appropriate format:
+    * width is a multiple of 8
+
+    """
+    w,h = im.size
+    msg = None
+    if w%8 != 0:
+        msg = "Image width is not a multiple of 8: {}".format(w)
+    if msg:
+        raise BadImageException(msg)
+
+def playfields(l):
+    pfs = []
+    pfs.append(list(reversed(l[0:4])) + 4*[False])
+    pfs.append(l[4:12])
+    pfs.append(list(reversed(l[12:20])))
+    pfs.append(list(reversed(l[20:24])) + 4*[False])
+    pfs.append(l[24:32])
+    pfs.append(list(reversed(l[32:40])))
+    return flatten(pfs)
+
+def main():
+    fname = sys.argv[1]
+    # Convert to 1 byte in {0,255} per pixel
+    im   = Image.open(fname)
+
+    # Beware im.convert('1') seems to introduce bugs !
+    # To be troubleshooted and fixed upstream !
+    # In the mean time using im.convert('L') instead
+    grey = im.convert('L')
+    sanity_check(grey)
+    arr   = bool_array(grey)
+    lines = [arr[i:i+40] for i in range(0, len(arr), 40)]
+    pfs   = [playfields(l) for l in lines]
+    pack  = pack_bytes(flatten(pfs))
+    #rev   = [~v & 0xff for v in pack]
+    print("pf_{}:".format(basename(fname).split(".")[0].replace("-","_")))
+    print(asmlib.lst2asm(pack, 6))
+
+main()
