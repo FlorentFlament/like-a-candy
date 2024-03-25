c5096bf8a42b09343beca6d33246dde363eecbba
Florent Flament <contact@florentflament.com>
2024-03-25 23:01:12 +0100

    More flexible number of rasterbars

--- src/dance_variables.asm
+++ src/dance_variables.asm
@@ -1,7 +1,7 @@
 beat_timer      ds.b    1       ; timer reseting to zero every beat
 beat_cnt        ds.b    1       ; Increases each beat
 
-dancebar_pos    ds.b    3       ; 3 bars
-dancebar_col    ds.b    3       ;
+dancebar_pos    ds.b    RASTERS_COUNT
+dancebar_col    ds.b    RASTERS_COUNT
 
 dance_bg        ds.b    24      ; 16 background lines to start with
