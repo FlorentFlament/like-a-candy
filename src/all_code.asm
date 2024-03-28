aed3fada74df7278ceacff9d3462f3e2579ccbcb
Florent Flament <contact@florentflament.com>
2024-03-28 15:16:27 +0100

    Minor text updates

--- src/all_code.asm
+++ src/all_code.asm
@@ -1,7 +1,7 @@
-        INCLUDE "text_code.asm"
         INCLUDE "fx_sprites_code.asm"
         INCLUDE "timeline_code.asm"
         INCLUDE "sync_code.asm"
+        INCLUDE "text_code.asm"
         INCLUDE "dance_code.asm"
 
 ;;; Music player wrapper
